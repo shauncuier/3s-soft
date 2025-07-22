2e37fc122f1a79b35589a1651049953512abcb6e
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-07-23 01:06:40 +0600

    fix: Update Vite server configuration to use headers instead of mimeTypes

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    mimeTypes: {
-      'js': 'text/javascript'
+    headers: {
+      'Content-Type': 'text/javascript'
     }
   },
 })
