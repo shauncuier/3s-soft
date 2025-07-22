8b5e4ce9e4aae12169a0a7a0a319e319521aa73c
Risan <borhansiddque19@gmail.com>
2025-07-23 01:47:22 +0600

    Navbar width change 1280px to 1480px

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   </>
   return (
     <div className="bg-black/30 backdrop-blur-lg sticky top-0 z-50 ">
-      <nav className="navbar max-w-7xl mx-auto">
+      <nav className="navbar max-w-[1480px] mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
