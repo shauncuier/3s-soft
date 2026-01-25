bcf2af37d6cbf0c953c6437416e543d8b626650f
Shaun <shauncuier@gmail.com>
2026-01-26 01:48:21 +0600

    Implement server-side configurations via .htaccess and add Vite build-time compression.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), cssInjectedByJsPlugin()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    cssInjectedByJsPlugin(),
+    viteCompression({ algorithm: 'gzip' }),
+    viteCompression({ algorithm: 'brotliCompress' })
+  ],
 })
